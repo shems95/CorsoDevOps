895c2f043284aa63111a8f908455fdf6f7526448
shems95 <61524292+shems95@users.noreply.github.com>
2023-04-03 04:46:29 +0200

    Only HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,8 @@
-import java.util.Map;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        Map<String, String> env = System.getenv();
-        for (String envName : env.keySet()) {
-            System.out.format("%s=%s%n",
-                    envName,
-                    env.get(envName));
+
     }
-}
+
 }
 
